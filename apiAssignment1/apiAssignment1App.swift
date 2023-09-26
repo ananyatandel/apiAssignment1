03fe0782f37cbecc151118bcec3900a508e59460
Ananya Tandel <anta1515@colorado.edu>
2023-09-25 23:52:58 -0600

    Initial Commit

---
+++ apiAssignment1/apiAssignment1App.swift
@@ -0,0 +1,17 @@
+//
+//  apiAssignment1App.swift
+//  apiAssignment1
+//
+//  Created by Ananya Tandel on 9/25/23.
+//
+
+import SwiftUI
+
+@main
+struct apiAssignment1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
